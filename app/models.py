ecbb7089a0bce9c2e2342f24558ca7612bb94e31
AyimWilliam <odhisayim@gmail.com>
2023-06-07 01:58:29 +0300

    Completed coach and team model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,62 @@
-from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%referred_table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+class Team(Base):
+    __tablename__ = 'teams'
+
+    id = Column(Integer(), primary_key = True)
+    team_name = Column(String())
+    played_games = Column(Integer, default = 38)
+    won_games = Column(Integer)
+    lost_games = Column(Integer)
+    drawn_games = Column(Integer)
+    goals_scored = Column(Integer)
+    goals_allowed = Column(Integer)
+    goal_difference = Column(Integer)
+    points = Column(Integer)
+
+    def __init__(self, team_name, played_games, won_games, 
+                 lost_games, drawn_games, goals_scored, 
+                 goals_allowed, points):
+        self.team_name = team_name
+        self.played_games = played_games
+        self.won_games = won_games
+        self.lost_games = lost_games
+        self.drawn_games = drawn_games
+        self.goals_scored = goals_scored
+        self.goals_allowed = goals_allowed
+        self.goal_difference = (goals_scored - goals_allowed)
+        self.points = (won_games*3)
+
+class Coach(Base):
+    __tablename__ = 'coaches'
+
+    id = Column(Integer(), primary_key = True)
+    first_name = Column(String())
+    second_name = Column(String())
+    nationality = Column(String())
+    age = Column(Integer())
+    team_id = Column(Integer())
+
+    def __init__(self, first_name, second_name, nationality, age, team_id):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.nationality = nationality
+        self.age = age
+        self.team_id = team_id
+
+class Player(Base):
+    __table
+
+
+
+Base.metadata.create_all(engine)
