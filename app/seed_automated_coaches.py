c4ace16f8da6ffa6afa04422851c80cf48f0a185
AyimWilliam <odhisayim@gmail.com>
2023-06-07 05:24:10 +0300

    finished seeding data for coaches

---
+++ app/seed_automated_coaches.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from faker import Faker
+from models import Team, Coach, Base
+
+engine = create_engine('sqlte:///fuata_dimba.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#creating a faker instance
+fake = Faker()
+
+#Define the team names to choose from
+
+team_names = ["Manchester City","Arsenal","Manchester United","Newcastle", "Liverpool",
+              "Brighton", "Aston Villa","Tottenham", "Brentford","Fulham",
+              "Crystal Palace", "Chelsea", "Wolves", "Westham", "Bournemouth", "Forest",
+              "Everton", "Leicester", "Leeds", "Southampton"]
+
+for c in range(20):
+    #create a coach
+    coach = Coach(
+        first_name = fake.first_name(),
+        second_name = fake.last_name(),
+        nationality = fake.country(),
+        age = fake.random_int(min=25, max=60),
+    )
+
+    #generate a random team
+    team_name = fake.random_element(elements=team_names)
+    team = session.query(Team).filter_by(team_name=team_name).first()
+
+    #assign coach to the team
+    team.coach = coach
+
+    #adding coach to the session
+    session.add(coach)
+
+#commit the changes to the database
+session.commit()
+
