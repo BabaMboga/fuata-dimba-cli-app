9aa184a03bb17df91c9ce036824ee1e7f1400cbc
AyimWilliam <odhisayim@gmail.com>
2023-06-09 02:23:55 +0300

    First migration done

---
+++ app/migrations/versions/fb82565ca974_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: fb82565ca974
+Revises: 
+Create Date: 2023-06-09 02:18:20.141262
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fb82565ca974'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- app/models.py
+++ app/models.py
@@ -10,85 +10,85 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
-team_coach = Table(
-    'team_coaches',
-    Base.metadata,
-    Column('team_id', ForeignKey('teams.id'), primary_key=True),
-    Column('coach_id', ForeignKey('coaches.id'), primary_key=True),
-    extend_existing=True
-)
+# team_coach = Table(
+#     'team_coaches',
+#     Base.metadata,
+#     Column('team_id', ForeignKey('teams.id'), primary_key=True),
+#     Column('coach_id', ForeignKey('coaches.id'), primary_key=True),
+#     extend_existing=True
+# )
 
-team_player = Table(
-    'team_players',
-    Base.metadata,
-    Column('team_id', ForeignKey('teams.id'), primary_key=True),
-    Column('player_id', ForeignKey('players.id'), primary_key=True),
-    extend_existing=True
-)
-
-
-class Team(Base):
-    __tablename__ = 'teams'
-
-    id = Column(Integer(), primary_key=True)
-    team_name = Column(String())
-    played_games = Column(Integer, default=38)
-    won_games = Column(Integer)
-    lost_games = Column(Integer)
-    drawn_games = Column(Integer)
-    goals_scored = Column(Integer)
-    goals_allowed = Column(Integer)
-    goal_difference = Column(Integer)
-    points = Column(Integer)
-
-    coach = relationship('Coach', backref=backref('team', uselist=False))
-
-    players = relationship('Player', secondary=team_player, backref=backref('teams', lazy='dynamic'))
+# team_player = Table(
+#     'team_players',
+#     Base.metadata,
+#     Column('team_id', ForeignKey('teams.id'), primary_key=True),
+#     Column('player_id', ForeignKey('players.id'), primary_key=True),
+#     extend_existing=True
+# )
 
 
-class Coach(Base):
-    __tablename__ = 'coaches'
+# class Team(Base):
+#     __tablename__ = 'teams'
 
-    id = Column(Integer(), primary_key=True)
-    full_name = Column(String())
-    nationality = Column(String())
-    age = Column(Integer())
+#     id = Column(Integer(), primary_key=True)
+#     team_name = Column(String())
+#     played_games = Column(Integer, default=38)
+#     won_games = Column(Integer)
+#     lost_games = Column(Integer)
+#     drawn_games = Column(Integer)
+#     goals_scored = Column(Integer)
+#     goals_allowed = Column(Integer)
+#     goal_difference = Column(Integer)
+#     points = Column(Integer)
 
-    team_id = Column(Integer(), ForeignKey('teams.id'))
+#     coach = relationship('Coach', backref=backref('team', uselist=False))
 
-    players = relationship('Player', backref=backref('coach'))
+#     players = relationship('Player', secondary=team_player, backref=backref('teams', lazy='dynamic'))
+
+
+# class Coach(Base):
+#     __tablename__ = 'coaches'
+
+#     id = Column(Integer(), primary_key=True)
+#     full_name = Column(String())
+#     nationality = Column(String())
+#     age = Column(Integer())
+
+#     team_id = Column(Integer(), ForeignKey('teams.id'))
+
+#     players = relationship('Player', backref=backref('coach'))
    
     
 
 
-class Player(Base):
-    __tablename__ = 'players'
+# class Player(Base):
+#     __tablename__ = 'players'
 
-    id = Column(Integer(), primary_key=True)
-    full_name = Column(String())
-    nationality = Column(String())
-    age = Column(String())
+#     id = Column(Integer(), primary_key=True)
+#     full_name = Column(String())
+#     nationality = Column(String())
+#     age = Column(String())
 
-    coach_id = Column(Integer(), ForeignKey('coaches.id'))
-    team_id = Column(Integer(), ForeignKey('teams.id'))
+#     coach_id = Column(Integer(), ForeignKey('coaches.id'))
+#     team_id = Column(Integer(), ForeignKey('teams.id'))
 
 
-class PlayerStat(Base):
-    __tablename__ = 'playerstats'
+# class PlayerStat(Base):
+#     __tablename__ = 'playerstats'
 
-    id = Column(Integer(), primary_key=True)
-    shots = Column(Integer())
-    goals = Column(Integer())
-    assists = Column(Integer())
-    goal_contributions = Column(Integer())
-    pass_completion_percentage = Column(Integer())
-    clean_sheets = Column(Integer())
-    conversion_rate = Column(Integer())
-    yellow_cards = Column(Integer())
-    red_cards = Column(Integer())
-    player_id = Column(Integer(), ForeignKey("players.id"), unique=True)
+#     id = Column(Integer(), primary_key=True)
+#     shots = Column(Integer())
+#     goals = Column(Integer())
+#     assists = Column(Integer())
+#     goal_contributions = Column(Integer())
+#     pass_completion_percentage = Column(Integer())
+#     clean_sheets = Column(Integer())
+#     conversion_rate = Column(Integer())
+#     yellow_cards = Column(Integer())
+#     red_cards = Column(Integer())
+#     player_id = Column(Integer(), ForeignKey("players.id"), unique=True)
 
-    player = relationship('Player', backref=backref('playerstat', uselist=False))
+#     player = relationship('Player', backref=backref('playerstat', uselist=False))
 
-engine = create_engine('sqlite:///fuata_dimba.db')
-Base.metadata.create_all(engine)
+# engine = create_engine('sqlite:///fuata_dimba.db')
+# Base.metadata.create_all(engine)
